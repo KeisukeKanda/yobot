ea379c41d98bb641f25dcb7f2786ac041bc5cc70
KeisukeKanda <keisukekanda@gmail.com>
2021-10-08 13:41:30 +0900

    喫煙を編集

--- resources/views/smoking.blade.php
+++ resources/views/smoking.blade.php
@@ -1,5 +1,4 @@
 <link href="{{ asset('css/primary_prevention.css') }}" rel="stylesheet">
-
 @extends('layouts.app')
 
 @section('content')
@@ -23,20 +22,40 @@
     <h3>これまであなたが喫煙してきた影響</h3>
 
     <div class="main">
-      <div class="brinkman_index">
-        <label for="number_of_smoke">1日の喫煙本数（平均）</label>
-        <input type="text"> 本
-        <label for="years_of_smoke">喫煙年数</label>
-        <input type="text"> 年
-        <input type="button" value="計 算">
-        <label for="risk">危険性</label>
-        <input type="text">
+      <div class="brinkman_calculator">
+        <div>
+          <label for="number_of_smoke">1日の喫煙本数（平均）</label>
+          <input type="number" id="num_smoke"> 本
+        </div>
+        <div>
+          <label for="years_of_smoke">喫煙年数</label>
+          <input type="number" id="years_smoke"> 年
+        </div>
+        <div>
+          <input type="button" value="計 算" id="calc_button" onclick="buttonClick()">
+        </div>
+        <div>
+          <label for="brinkman_index">ブリンクマン指数</label>
+          <input type="number" id="brinkman_result">
+        </div>
       </div>
+    </div>
 
-
-
+    <div class="brinkman_evaluation">
+      <div>400以上：肺がんを発症する危険性が高い</div>
+      <div>600以上：肺がんを発症する危険性がかなり高い</>
+      <div>1200以上：喉頭癌を発症する危険性がかなり高い</div>
     </div>
   </div>
   <div class="back"><a class="btn btn-primary" href="{{ asset('/primary_prevention') }}">戻る</a></div>
 </div>
+
+<script>
+  function buttonClick() {
+    const number_of_smoke = document.getElementById("num_smoke").value;
+    const years_of_smoke = document.getElementById("years_smoke").value;
+    const brinkman_index = number_of_smoke * years_of_smoke;
+    document.getElementById("brinkman_result").value = brinkman_index;
+  }
+</script>
 @endsection
