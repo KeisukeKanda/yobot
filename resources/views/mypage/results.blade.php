a2440c4143f83f25106e3f4bd62a8506ced17f51
KeisukeKanda <keisukekanda@gmail.com>
2021-10-11 07:15:21 +0900

    resultの画像にassetを追記

--- resources/views/mypage/results.blade.php
+++ resources/views/mypage/results.blade.php
@@ -9,11 +9,15 @@
 <div class="wrap">
     <h2>{{ $item->item_name }}の結果</h2>
     <div class="main">
-        <div class="done_date"><p class="date">{{ $result->done_date }}</p></div>
-        <div class="result_img"><img src="/result_upload/{{$result->result_file}}" class="img"></div>
+        <div class="done_date">
+            <p class="date">{{ $result->done_date }}</p>
+        </div>
+        <div class="result_img"><img src="{{ asset('/result_upload/'.$result->result_file.'') }}" class="img"></div>
         <div class="result_comment"></div>
         <label>自己記録（コメント・メモ）</label>
-        <div class="comment_frame"><p class="comment">{{ $result->result_comment }}</p></div>
+        <div class="comment_frame">
+            <p class="comment">{{ $result->result_comment }}</p>
+        </div>
     </div>
     <div class="detail_button">
         <div class="back_button"><a class="btn btn-primary" href="{{ url('/mypage/done') }}">戻る</a></div>
