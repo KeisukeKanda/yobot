8aa508db01f44088ada27c41a9b6309291e81079
KeisukeKanda <keisukekanda@gmail.com>
2021-10-21 11:11:52 +0900

    “smoking追記”

--- resources/views/vaccination.blade.php
+++ resources/views/vaccination.blade.php
@@ -3,15 +3,23 @@
 @extends('layouts.app')
 
 @section('content')
-
-現在開発中
-
 <div class="wrap">
   <div class="top_container">
     <div class="top">
       <div class="item_img"><img src="{{ asset('/upload/hu-chen-tCbTGNwrFNM-unsplash.jpg') }}" class="img"></div>
     </div>
   </div>
+
+  <h3>Vaccination</h3>
+
+  <p>予防接種は<br>自分自身が病気にかかりにくくなるだけではなく<br>社会全体でも流行を防ぐ効果があります</p>
+  <p>予防接種のおかげで<br>これまで命に関わったり障害の原因となっていた<br>重篤な感染症も防ぐことができるようになりました</p>
+  <p>必要な予防接種を受けることは<br>一次予防において大切なことです</p>
+
+  <div class="main_container">
+    <h3>大人の予防接種</h3>
+  </div>
+
 </div>
 
-@endsection
+  @endsection
